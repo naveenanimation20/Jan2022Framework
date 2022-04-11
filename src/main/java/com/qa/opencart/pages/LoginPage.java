b940912d1da2aa47b24c6aede14585fe359a32e9
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-04-11 06:39:47 +0400

    updated code and deleted file

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -21,7 +21,6 @@ public class LoginPage {
 	private By loginBtn = By.xpath("//input[@value='Login']");
 	private By forgotPwd = By.linkText("Forgotten Password11");
 	private By registerLink = By.linkText("Register");
-	private By myRepo = By.linkText("My Repo");
 	private By loginErrorMessg = By.cssSelector("div.alert.alert-danger.alert-dismissible");
 
 
