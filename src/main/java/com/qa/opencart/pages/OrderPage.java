e6cc55c339b68d1050ac14df36c1ad059bd0f2ac
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-04-13 06:42:08 +0400

    Create OrderPage.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,20 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+	
+  	public void getOrderId() {
+		System.out.println("order id 1234....");
+	}
+  
+	
+	public void makeOrder() {
+		System.out.println("order page....");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
