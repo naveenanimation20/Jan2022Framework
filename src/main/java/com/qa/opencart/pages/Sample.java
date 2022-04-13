92262495d15db86d3bc345946e2056758691e766
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-04-13 06:32:50 +0400

    sample naveen code

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+	
+	
+	public void login() {
+		String username = "naveen";
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
