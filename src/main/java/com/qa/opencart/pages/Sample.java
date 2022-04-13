18e113079a7cb1f2479e11f40913633e8d47e8df
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-04-13 06:31:08 +0400

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,18 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+	
+	
+	public void login() {
+		String username = "Tom";
+    String password = "Tom123";
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
